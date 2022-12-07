cea619cebab5ce5d194e1bc1171ac539c128e47e
Skanner33 <mathewmageto@gmail.com>
2022-12-07 12:22:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData (name, email){
+let users= {    
+    name:name,
+    email:email,
+};
+
+let configurationObject = {
+ method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(users),
+};
+
+return fetch("http://localhost:3000/users", configurationObject)
+.then(function (response) {
+    return response.json();
+})
+.then(function (object) {
+    let h3 = document.createElement('h3')
+    h3.innerHTML = object.id;
+    document.body.appendChild(h3)
+    console.log(object);
+})
+.catch(function (error){
+    let h3 = document.createElement('h3')
+    document.body.appendChild(h3)
+    h3.innerHTML = error.message;
+    console.log(error.message)
+})
+};
+submitData('luca', 'luca@gmail.com');
